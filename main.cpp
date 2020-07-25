2dce49f536737d8a7b283a045a6be69b0d4d0cbb
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2020-07-24 22:07:33 -0400

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QtCharts/QChartView>
-#include <QtCharts/QLineSeries>
-#include <QtCharts/QCategoryAxis>
-#include <QtCharts/QLegend>
-#include <QtWebEngine>
-//#include <QtWebEngineCore>
-#include <QtWebEngineWidgets>
 
-
-QT_CHARTS_USE_NAMESPACE
 int main(int argc, char *argv[])
 {
-
-    QtWebEngine::initialize();
     QApplication a(argc, argv);
-    QNetworkProxyFactory::setUseSystemConfiguration(true);
-
     MainWindow w;
     w.show();
-
-
     return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,8 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <apiauthenticator.h>
-#include <wifiapi.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,27 +14,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    ApiAuthenticator apiAuth;
-    QString accessTkn;
-    QString apiTkn;
-private slots:
-    void on_actionSign_In_triggered();
-
-    void on_actionServer_Tokens_triggered();
-
-    void on_devicesBtn_clicked();
 
 private:
-    QString refreshToken;
-    WifiApi::System *system;
-    WifiApi *client;
-
     Ui::MainWindow *ui;
-
-    void refreshUi();
-
-    void startApiService();
-
-    void authComplete(QString refreshTkn);
 };
 #endif // MAINWINDOW_H
